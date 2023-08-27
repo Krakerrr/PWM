7ea4a6d2bf8ef6a5be6722d2cebb118987ab1b20
ali.keskin <ali.keskin@stm.com.tr>
2023-08-28 01:06:15 +0300

    bug fix

--- Inc/PWM.h
+++ Inc/PWM.h
@@ -11,14 +11,26 @@
 #include "main_app.h"
 
 
-#define PWM_INSTANCE 		TIM2
-#define PWM_CHANNEL_1		TIM_CHANNEL_1
-#define PWM_CHANNEL_2		TIM_CHANNEL_2
-#define PWM_CHANNEL_3		TIM_CHANNEL_3
-#define PWM_CHANNEL_4		TIM_CHANNEL_4
-#define MAX_PWM_USEC 		2100
-#define CNTR_PWM_USEC 		1500
-#define MIN_PWM_USEC 		900
+#define PWM_INSTANCE 			TIM2
+#define PWM_CHANNEL_1			TIM_CHANNEL_1
+#define PWM_CHANNEL_2			TIM_CHANNEL_2
+#define PWM_CHANNEL_3			TIM_CHANNEL_3
+#define PWM_CHANNEL_4			TIM_CHANNEL_4
+#define PWM_GPIO				GPIOA
+#define PWM_GPIO_PINS			GPIO_PIN_0 | GPIO_PIN_1
+//#define PWM_GPIO_PINS			GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3
+#define PWM_GPIO_AF        		GPIO_AF1_TIM2
+#define MAX_PWM_USEC 			2100
+#define CNTR_PWM_USEC 			1500
+#define MIN_PWM_USEC 			900
+#define __PWM_CLK_ENABLE()   	do { \
+										__HAL_RCC_TIM2_CLK_ENABLE(); \
+										__HAL_RCC_GPIOA_CLK_ENABLE(); \
+								} while(0U)
+#define __PWM_NVIC_ENABLE()   	do { \
+										HAL_NVIC_SetPriority(USART1_IRQn, 15, 0); \
+										HAL_NVIC_EnableIRQ(USART1_IRQn); \
+								} while(0U)
 
 TIM_HandleTypeDef hPWM;
 
@@ -29,9 +41,12 @@ TIM_HandleTypeDef hPWM;
  * 	0.9ms min / 1.5ms center / 2.1ms max
  */
 void PWM_Timer_Initialization(void);
-void PWM_GPIO_Initialization(void);
+void PWM_GPIO_Initialization(TIM_HandleTypeDef *htim);
 void PWM_Initialization(void);
 void PWM_SET_CHANNEL1(uint16_t pwm_microsec);
+void PWM_SET_CHANNEL2(uint16_t pwm_microsec);
+void PWM_SET_CHANNEL3(uint16_t pwm_microsec);
+void PWM_SET_CHANNEL4(uint16_t pwm_microsec);
 
 
 
--- Src/PWM.c
+++ Src/PWM.c
@@ -8,10 +8,6 @@
 
 #include "PWM.h"
 
-
-
-
-
 void PWM_Timer_Initialization(void)
 {
 	TIM_OC_InitTypeDef tim2PWM_Config = {0};
@@ -36,51 +32,44 @@ void PWM_Timer_Initialization(void)
 		Error_Handler();
 	}
 
-	//	if( HAL_TIM_PWM_ConfigChannel(&hPWM, &tim2PWM_Config, TIM_CHANNEL_2) != HAL_OK )
-	//	{
-	//		UARTprintmsg("HAL_TIM_PWM_ConfigChannel2 error \r\n");
-	//		Error_handler();
-	//	}
+	if( HAL_TIM_PWM_ConfigChannel(&hPWM, &tim2PWM_Config, TIM_CHANNEL_2) != HAL_OK )
+	{
+		UARTprintmsg("HAL_TIM_PWM_ConfigChannel2 error \r\n");
+		Error_Handler();
+	}
 	//
 	//	if( HAL_TIM_PWM_ConfigChannel(&hPWM, &tim2PWM_Config, TIM_CHANNEL_3) != HAL_OK )
 	//	{
 	//		UARTprintmsg("HAL_TIM_PWM_ConfigChannel3 error \r\n");
-	//		Error_handler();
+	//		Error_Handler();
 	//	}
 	//
 	//	if( HAL_TIM_PWM_ConfigChannel(&hPWM, &tim2PWM_Config, TIM_CHANNEL_4) != HAL_OK )
 	//	{
 	//		UARTprintmsg("HAL_TIM_PWM_ConfigChannel4 error \r\n");
-	//		Error_handler();
+	//		Error_Handler();
 	//	}
 }
 
-
-
-
-void PWM_GPIO_Initialization(void)
+void PWM_GPIO_Initialization(TIM_HandleTypeDef *htim)
 {
-	GPIO_InitTypeDef tim2oc_ch_gpios;
-	// enable peripheral clocks for timer2
-	__HAL_RCC_TIM2_CLK_ENABLE();
-	__HAL_RCC_GPIOA_CLK_ENABLE();
+	if( htim->Instance == PWM_INSTANCE)
+	{
+		GPIO_InitTypeDef tim2oc_ch_gpios;
+		// enable peripheral clocks for timer2
+		__PWM_CLK_ENABLE();
 
-	// config gpios to behave as timer2 channel 1-2-3-4
-	//	PA0 -> TIM2_CH1
-	//	PA1 -> TIM2_CH2
-	//	PA2 -> TIM2_CH3
-	//	PA3 -> TIM2_CH4
-	//	tim2oc_ch_gpios.Pin 	= GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 ;
-	tim2oc_ch_gpios.Pin 		= GPIO_PIN_0 ;
-	tim2oc_ch_gpios.Mode 		= GPIO_MODE_AF_PP;
-	tim2oc_ch_gpios.Pull   	 	= GPIO_NOPULL;
-	tim2oc_ch_gpios.Speed   	= GPIO_SPEED_FREQ_HIGH;
-	tim2oc_ch_gpios.Alternate 	= GPIO_AF1_TIM2;
-	HAL_GPIO_Init(GPIOA, &tim2oc_ch_gpios);
+		// config gpios to behave as timer2 channel 1-2-3-4
+		tim2oc_ch_gpios.Pin 		= PWM_GPIO_PINS;
+		tim2oc_ch_gpios.Mode 		= GPIO_MODE_AF_PP;
+		tim2oc_ch_gpios.Pull   	 	= GPIO_NOPULL;
+		tim2oc_ch_gpios.Speed   	= GPIO_SPEED_FREQ_HIGH;
+		tim2oc_ch_gpios.Alternate 	= PWM_GPIO_AF;
+		HAL_GPIO_Init(PWM_GPIO, &tim2oc_ch_gpios);
 
-	// nvic setting
-	HAL_NVIC_SetPriority(TIM2_IRQn, 15, 0);
-	HAL_NVIC_EnableIRQ(TIM2_IRQn);
+		// nvic setting
+		__PWM_NVIC_ENABLE();
+	}
 }
 
 void PWM_Initialization(void)
@@ -91,6 +80,28 @@ void PWM_Initialization(void)
 		UARTprintmsg("HAL_TIM_PWM_Start error \r\n");
 		Error_Handler();
 	}
+
+	if( HAL_TIM_PWM_Start(&hPWM, TIM_CHANNEL_2) != HAL_OK )
+	{
+		UARTprintmsg("HAL_TIM_PWM_Start error \r\n");
+		Error_Handler();
+	}
+
+//	if( HAL_TIM_PWM_Start(&hPWM, TIM_CHANNEL_3) != HAL_OK )
+//	{
+//		UARTprintmsg("HAL_TIM_PWM_Start error \r\n");
+//		Error_Handler();
+//	}
+//
+//	if( HAL_TIM_PWM_Start(&hPWM, TIM_CHANNEL_4) != HAL_OK )
+//	{
+//		UARTprintmsg("HAL_TIM_PWM_Start error \r\n");
+//		Error_Handler();
+//	}
+
+	PWM_SET_CHANNEL1(CNTR_PWM_USEC);
+
+	PWM_SET_CHANNEL2(CNTR_PWM_USEC);
 }
 
 void PWM_SET_CHANNEL1(uint16_t pwm_microsec)
@@ -100,4 +111,31 @@ void PWM_SET_CHANNEL1(uint16_t pwm_microsec)
 	if(pwm_microsec < MIN_PWM_USEC)
 		pwm_microsec = MIN_PWM_USEC;
 	__HAL_TIM_SET_COMPARE(&hPWM, PWM_CHANNEL_1, pwm_microsec);
+}
+
+void PWM_SET_CHANNEL2(uint16_t pwm_microsec)
+{
+	if(pwm_microsec >MAX_PWM_USEC)
+		pwm_microsec = MAX_PWM_USEC;
+	if(pwm_microsec < MIN_PWM_USEC)
+		pwm_microsec = MIN_PWM_USEC;
+	__HAL_TIM_SET_COMPARE(&hPWM, PWM_CHANNEL_2, pwm_microsec);
+}
+
+void PWM_SET_CHANNEL3(uint16_t pwm_microsec)
+{
+	if(pwm_microsec >MAX_PWM_USEC)
+		pwm_microsec = MAX_PWM_USEC;
+	if(pwm_microsec < MIN_PWM_USEC)
+		pwm_microsec = MIN_PWM_USEC;
+	__HAL_TIM_SET_COMPARE(&hPWM, PWM_CHANNEL_3, pwm_microsec);
+}
+
+void PWM_SET_CHANNEL4(uint16_t pwm_microsec)
+{
+	if(pwm_microsec >MAX_PWM_USEC)
+		pwm_microsec = MAX_PWM_USEC;
+	if(pwm_microsec < MIN_PWM_USEC)
+		pwm_microsec = MIN_PWM_USEC;
+	__HAL_TIM_SET_COMPARE(&hPWM, PWM_CHANNEL_4, pwm_microsec);
 }
