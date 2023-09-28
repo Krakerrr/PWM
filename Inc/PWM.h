c6229d79438b211ac1ac06835bdf981379eb7403
Ali <alikskn91@gmail.com>
2023-09-28 22:44:36 +0300

    #1 Timer freq edited, error handler edited

--- Inc/PWM.h
+++ Inc/PWM.h
@@ -2,7 +2,7 @@
  * PWM.h
  *
  *  Created on: Aug 17, 2023
- *      Author: Administrator
+ *      Author: Kraker
  */
 
 #ifndef INC_PWM_H_
@@ -17,8 +17,7 @@
 #define PWM_CHANNEL_3			TIM_CHANNEL_3
 #define PWM_CHANNEL_4			TIM_CHANNEL_4
 #define PWM_GPIO				GPIOA
-#define PWM_GPIO_PINS			GPIO_PIN_0 | GPIO_PIN_1
-//#define PWM_GPIO_PINS			GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3
+#define PWM_GPIO_PINS			GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3
 #define PWM_GPIO_AF        		GPIO_AF1_TIM2
 #define MAX_PWM_USEC 			2100
 #define CNTR_PWM_USEC 			1500
@@ -26,10 +25,6 @@
 #define __PWM_CLK_ENABLE()   	do { \
 										__HAL_RCC_TIM2_CLK_ENABLE(); \
 										__HAL_RCC_GPIOA_CLK_ENABLE(); \
-								} while(0U)
-#define __PWM_NVIC_ENABLE()   	do { \
-										HAL_NVIC_SetPriority(USART1_IRQn, 15, 0); \
-										HAL_NVIC_EnableIRQ(USART1_IRQn); \
 								} while(0U)
 
 TIM_HandleTypeDef hPWM;
--- Src/PWM.c
+++ Src/PWM.c
@@ -2,7 +2,7 @@
  * PWM.c
  *
  *  Created on: Aug 17, 2023
- *      Author: Administrator
+ *      Author: Kraker
  */
 
 
@@ -11,16 +11,15 @@
 void PWM_Timer_Initialization(void)
 {
 	TIM_OC_InitTypeDef tim2PWM_Config = {0};
-	// 50 HZ for 50mhz clock
+	// TODO : edit period for MCU freq
+	// 50 HZ for 168mhz clock
 	hPWM.Instance 			= PWM_INSTANCE;
-	hPWM.Init.Prescaler 	= 49;
+	hPWM.Init.Prescaler 	= 83;
 	hPWM.Init.Period		= 20000-1;
 	if ( HAL_TIM_PWM_Init(&hPWM) != HAL_OK)
 	{
-		RF_SendMsg("PWM_Init error \r\n");
-		Error_Handler();
+		Error_Handler(E_PWMInit);
 	}
-
 
 	tim2PWM_Config.OCMode     	= TIM_OCMODE_PWM1;
 	tim2PWM_Config.OCPolarity 	= TIM_OCPOLARITY_HIGH;
@@ -28,27 +27,23 @@ void PWM_Timer_Initialization(void)
 
 	if( HAL_TIM_PWM_ConfigChannel(&hPWM, &tim2PWM_Config, TIM_CHANNEL_1) != HAL_OK )
 	{
-		RF_SendMsg("HAL_TIM_PWM_ConfigChannel1 error \r\n");
-		Error_Handler();
+		Error_Handler(E_PWMConfigChannel1);
 	}
 
 	if( HAL_TIM_PWM_ConfigChannel(&hPWM, &tim2PWM_Config, TIM_CHANNEL_2) != HAL_OK )
 	{
-		RF_SendMsg("HAL_TIM_PWM_ConfigChannel2 error \r\n");
-		Error_Handler();
+		Error_Handler(E_PWMConfigChannel2);
 	}
-	//
-	//	if( HAL_TIM_PWM_ConfigChannel(&hPWM, &tim2PWM_Config, TIM_CHANNEL_3) != HAL_OK )
-	//	{
-	//		RF_SendMsg("HAL_TIM_PWM_ConfigChannel3 error \r\n");
-	//		Error_Handler();
-	//	}
-	//
-	//	if( HAL_TIM_PWM_ConfigChannel(&hPWM, &tim2PWM_Config, TIM_CHANNEL_4) != HAL_OK )
-	//	{
-	//		RF_SendMsg("HAL_TIM_PWM_ConfigChannel4 error \r\n");
-	//		Error_Handler();
-	//	}
+
+	if( HAL_TIM_PWM_ConfigChannel(&hPWM, &tim2PWM_Config, TIM_CHANNEL_3) != HAL_OK )
+	{
+		Error_Handler(E_PWMConfigChannel3);
+	}
+
+	if( HAL_TIM_PWM_ConfigChannel(&hPWM, &tim2PWM_Config, TIM_CHANNEL_4) != HAL_OK )
+	{
+		Error_Handler(E_PWMConfigChannel4);
+	}
 }
 
 void PWM_GPIO_Initialization(TIM_HandleTypeDef *htim)
@@ -66,9 +61,6 @@ void PWM_GPIO_Initialization(TIM_HandleTypeDef *htim)
 		tim2oc_ch_gpios.Speed   	= GPIO_SPEED_FREQ_HIGH;
 		tim2oc_ch_gpios.Alternate 	= PWM_GPIO_AF;
 		HAL_GPIO_Init(PWM_GPIO, &tim2oc_ch_gpios);
-
-		// nvic setting
-		__PWM_NVIC_ENABLE();
 	}
 }
 
@@ -77,31 +69,31 @@ void PWM_Initialization(void)
 	PWM_Timer_Initialization();
 	if( HAL_TIM_PWM_Start(&hPWM, TIM_CHANNEL_1) != HAL_OK )
 	{
-		RF_SendMsg("HAL_TIM_PWM_Start error \r\n");
-		Error_Handler();
+		Error_Handler(E_PWMStartCH1);
 	}
 
 	if( HAL_TIM_PWM_Start(&hPWM, TIM_CHANNEL_2) != HAL_OK )
 	{
-		RF_SendMsg("HAL_TIM_PWM_Start error \r\n");
-		Error_Handler();
+		Error_Handler(E_PWMStartCH2);
 	}
 
-//	if( HAL_TIM_PWM_Start(&hPWM, TIM_CHANNEL_3) != HAL_OK )
-//	{
-//		RF_SendMsg("HAL_TIM_PWM_Start error \r\n");
-//		Error_Handler();
-//	}
-//
-//	if( HAL_TIM_PWM_Start(&hPWM, TIM_CHANNEL_4) != HAL_OK )
-//	{
-//		RF_SendMsg("HAL_TIM_PWM_Start error \r\n");
-//		Error_Handler();
-//	}
+	if( HAL_TIM_PWM_Start(&hPWM, TIM_CHANNEL_3) != HAL_OK )
+	{
+		Error_Handler(E_PWMStartCH3);
+	}
+
+	if( HAL_TIM_PWM_Start(&hPWM, TIM_CHANNEL_4) != HAL_OK )
+	{
+		Error_Handler(E_PWMStartCH4);
+	}
 
 	PWM_SET_CHANNEL1(CNTR_PWM_USEC);
 
 	PWM_SET_CHANNEL2(CNTR_PWM_USEC);
+
+	PWM_SET_CHANNEL3(CNTR_PWM_USEC);
+
+	PWM_SET_CHANNEL4(CNTR_PWM_USEC);
 }
 
 void PWM_SET_CHANNEL1(uint16_t pwm_microsec)
